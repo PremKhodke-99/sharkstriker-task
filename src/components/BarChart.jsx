47277f0bde0d53883cfdf5ba0b29f385e4848a0e
Prem <premkhodke.99@gmail.com>
2024-07-19 16:00:02 +0530

    bar

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
 
-const BarChart = () => {
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
+const BarChart = ({barwidth, barheight}) => {
+  const data = {
+    labels: ["0", "10", "20", "30", "40", "50", "60", "70", "80", "90", "100"],
+    datasets: [
+      {
+        label: "Severity",
+        data: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+      },
+    ],
+  };
+  const options = {
+    scales: {
+      xAxes: [
+        {
+          gridLines: {
+            display: true,
+            drawBorder: true,
+            borderDash: [4, 4],
+          },
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+      yAxes: [
+        {
+          display: true,
+          gridLines: {
+            display: true,
+            drawBorder: true,
+            borderDash: [2, 2],
+          },
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
+
   return (
-    <div>BarChart</div>
-  )
+      <Bar width={barwidth} height={barheight} data={data} options={options} />
+  );
 }
 
 export default BarChart
