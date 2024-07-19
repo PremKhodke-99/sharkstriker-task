ff0c877b58fb1907304a908c3871fba6ac93e89d
Prem <premkhodke.99@gmail.com>
2024-07-19 13:24:07 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import Box from "./components/Box"
+import Earth from "./components/Earth"
 import Navbar from "./components/Navbar"
+// import { RadarChart } from "./components/RadarChart"
 import SearchAndFilter from "./components/SearchAndFilter"
+import Table from "./components/Table"
 
 function App() {
 
@@ -7,6 +11,93 @@ function App() {
     <div>
       <Navbar />
       <SearchAndFilter />
+      <div className="grid-view">
+        <div className="left">
+          <div className="top">
+            <img src="back.png" alt="back" />
+            <h5>Top Users</h5>
+          </div>
+          <div className="radar">
+            <p>Current Top Suspect Assets{' '}
+              <select>
+                <option>25</option>
+              </select>
+            </p>
+            {/* <RadarChart /> */}
+          </div>
+          <p className="para">Expand All</p>
+        </div>
+
+        <div className="middle">
+          <div className="top">
+            <Box
+              title={'ml-ids'}
+              content={'2 / 2 / 21K'}
+              classN='top-left'
+            />
+            <Box
+              title={'traffic anomalies'}
+              content={'31 / 220'}
+            />
+            <Box
+              title={'malware sandbox'}
+              content={'0 / 0'}
+              classN='top-right'
+            />
+          </div>
+
+          <div className="center">
+            <div className="circle">
+              <img src="meter.png" alt="meter" />
+              <div className="earth">
+                <Earth />
+              </div>
+            </div>
+          </div>
+
+          <div className="bottom">
+            <Box
+              title={'found assets'}
+              content={'0 / 4 / 1715'}
+              classN='bottom-left'
+            />
+            <Box
+              title={'uba detections'}
+              content={'0 / 0'}
+            />
+            <Box
+              title={'daily data injested'}
+              content={'11.78 / 1000'}
+              classN='bottom-right'
+            />
+          </div>
+        </div>
+
+        <div className="right">
+          <div className="bar">
+            <p>Top Security Events{' '}
+              <select>
+                <option>100</option>
+              </select>
+            </p>
+            <img src="" alt="bar" />
+          </div>
+          <p className="para">Collapse All</p>
+
+          <h5>🔑 Private to Private Exploit Anomaly{' '}<span>77 Score</span></h5>
+          <Table />
+        </div>
+      </div>
+
+      <div className="border">
+        <div className="inner-box">
+          <div className="head">
+            <h5>Security Events Over Time</h5>
+            <img src="graph-ico.png" />
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
